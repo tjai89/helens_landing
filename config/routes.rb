7c2e5f4d999a4f5dd3efea631d4255c9439b13cd
Kirill Zhuravlov <tjai89@mail.ru>
2016-09-19 02:07:00 +0300

    add ru locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'contacts#new'
   resources "contacts", only: [:new, :create]
 
-  # You can have the root of your site routed with "root"
-  root 'contacts#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
