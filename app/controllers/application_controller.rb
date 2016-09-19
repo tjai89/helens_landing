a83da4171f0e9b09a83266f2545b4ff176a5453f
Kirill Zhuravlov <tjai89@mail.ru>
2016-09-19 22:27:42 +0300

    Add locale to domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_locale
-
-private
-  
-
+  before_action :set_locale
+   
   def set_locale
-    I18n.locale = params[:locale] if params[:locale].present?
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
   end
-
+   
+  # Получаем локаль из домена верхнего уровня или возвращаем nil, если такая локаль недоступна
+  # Вам следует поместить что-то наподобие этого:
+  #   127.0.0.1 application.com
+  #   127.0.0.1 application.it
+  #   127.0.0.1 application.pl
+  # в ваш файл /etc/hosts, чтобы попробовать это локально
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources "contacts", only: [:new, :create]
 
 
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
