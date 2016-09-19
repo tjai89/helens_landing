91f40e528e6428abcbd61a650eeedbea115fdeb3
Kirill Zhuravlov <tjai89@mail.ru>
2016-09-19 23:21:36 +0300

    Add shared folder end render shared/_

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
    
   def set_locale
-    I18n.locale = extract_locale_from_tld || I18n.default_locale
-  end
-   
-  # Получаем локаль из домена верхнего уровня или возвращаем nil, если такая локаль недоступна
-  # Вам следует поместить что-то наподобие этого:
-  #   127.0.0.1 application.com
-  #   127.0.0.1 application.it
-  #   127.0.0.1 application.pl
-  # в ваш файл /etc/hosts, чтобы попробовать это локально
-  def extract_locale_from_tld
-    parsed_locale = request.host.split('.').last
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'contacts#new'
-  resources "contacts", only: [:new, :create]
-
-
-
+  scope "(:locale)", locale: /en|ru/ do
+    root 'contacts#new'
+    resources "contacts", only: [:new, :create]
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
